d51e3410c6625cf64b299659ef6c189a86bcb474
OnyinyeIsaac <orjigraceonyinyechi@.com>
2023-01-02 13:55:45 -0800

    Prints the numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,30 +2,20 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char c;
+	int num;
+	char la;
 
-	int d;
+	for (num = o; num < 10; num++)
+		putchar((num % 10) + '0');
 
-	c = 'a';
-	d = '0';
-	while
-		(d < 10)
-		{
-			putchar(d + '0');
-			d++;
-		}
-	while
-	(c <= 'f')
-		{
-			putchar(c);
-			c++;
-		}
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
 	putchar('\n');
 	return (0);
 }
