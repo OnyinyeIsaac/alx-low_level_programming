d3cd2aceafc5cb3e7740e9cfad789876cfce49eb
OnyinyeIsaac <orjigraceonyinyechi@.com>
2023-01-03 10:43:57 -0800

    Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,14 +14,14 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
-	printf("%d", j);
+	printf("%ld", j);
 	else if (i == 1)
-	printf(", %d", k);
+	printf(", %ld", k);
 	else
 	{
 	k += j;
 	j = k - j;
-	printf(", %d", k);
+	printf(", %ld", k);
 	}
 	++i;
 	}
