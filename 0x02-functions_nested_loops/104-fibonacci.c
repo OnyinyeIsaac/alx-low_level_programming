53a4bb896df2791b2b498fd732200c942faff1ec
OnyinyeIsaac <orjigraceonyinyechi@.com>
2023-01-03 11:16:38 -0800

    first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,7 +31,7 @@ int main(void)
 	half2 = fib1_half2 + fib2_half2;
 	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
-	half1 <= 1;
+	half1 += 1;
 	half2 %= 10000000000;
 	}
 	printf("%lu%lu", half1, half2);
