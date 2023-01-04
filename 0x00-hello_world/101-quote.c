a29f1c42d8beda86601b4dbc4db6ac15af2fb9a6
OnyinyeIsaac <orjigraceonyinyechi@.com>
2023-01-04 12:54:44 -0800

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return:1
+ * Return: 1
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
-return (1);
+ char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ write(2, ch, sizeof(ch));
+ return (1);
 }
